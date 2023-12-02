aea357161c090872c3cb85b1bb7521685f43ee5e
Chris Mann <110433364+cmann80@users.noreply.github.com>
2023-12-02 00:07:17 +0000

    added Bing-created liguistic data but has flaws

--- jsonl_converter.py
+++ jsonl_converter.py
@@ -1,9 +1,9 @@
 import json
 
-with open('prepareddata.json', 'r') as f:
+with open('linguistics_fine_tuning_set.json', 'r') as f:
     data = json.load(f)
 
-with open('prepareddata.jsonl', 'w') as f:
+with open('linguistics_fine_tuning_set.jsonl', 'w') as f:
     for obj in data:
         json.dump(obj, f)
         f.write('\n')
